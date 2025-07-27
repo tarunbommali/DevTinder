15d52c9f5842d2e0f4361d507a0154bacd020b35
Tarun <btaruntej143@gmail.com>
2025-07-27 09:31:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <span className="text-blue-500"> Welcome to DevTinder!</span>
+      <span className="text-red-500">
+        I am Tarun Bommali, i'am building something amazing!
+      </span>
+    </h1>
+  );
+}
+
+export default App;
